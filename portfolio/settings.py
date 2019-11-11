84e8769d6bc2abb7616580f2a45da07b13654dfc
imsharvanj <imsharvanj@gmail.com>
2019-11-11 20:56:10 +0530

    database default values change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -94,12 +94,12 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'django1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'portfoliodb',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'django1234',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
